139e9d4dc1a1390fb8ddeeecede2e196b6826470
Thomas Tufta Løberg <thtech93@gmail.com>
2022-06-22 00:39:09 +0200

    Clean slate

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.scss';
 import App from './js/App';
+/*import * as calc from './js/functions.js'; */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
